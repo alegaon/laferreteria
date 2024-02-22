1ed839b3706c794bfc74dfd5159ab53f4910cd61
Ale <alegaon@outlook.com.ar>
2024-02-22 02:24:51 -0300

    fix(.gitignore): add modify .gitignore and add model, serializers

--- back/app_ferreteria/models.py
+++ back/app_ferreteria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CardSelling(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(max_length=None)
+    description = models.CharField(max_length=200)
+    publication_time = models.DateTimeField()
+    sold =models.BooleanField(default=False)
